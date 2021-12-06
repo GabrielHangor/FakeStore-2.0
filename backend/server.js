686e23b52948a295a66a57424774b62e947258bc
Gabriel <gaba93@yandex.ru>
2021-12-06 14:43:05 +0300

    connecting to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import { config } from 'dotenv';
+import connectToDB from './config/db.js';
 import products from './data/products.js';
 
 config();
+
+connectToDB();
 
 const app = express();
 
