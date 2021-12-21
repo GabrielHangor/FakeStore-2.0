3d51bfb570cb6e8e1aa94e637be16b473ae69a2d
Gabriel <gaba93@yandex.ru>
2021-12-21 11:31:17 +0300

    auth middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { authUser } from '../controllers/userController.js';
+import { authUser, getUserProfile } from '../controllers/userController.js';
+import { protectRoute } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 // routes handling user authentication
 
 router.route('/login').post(authUser);
+router.route('/profile').get(protectRoute, getUserProfile);
 
 export default router;
