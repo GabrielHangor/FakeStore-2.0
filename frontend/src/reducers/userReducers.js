0fb777545db78a835ecacda9cabf0c09ba6b149a
Gabriel <gaba93@yandex.ru>
2021-12-24 16:25:15 +0300

    getUserDetails reducer/action, update profile screen

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -7,6 +7,10 @@ export const USER_REGISTER_REQUEST = 'USER_REGISTER_REQUEST';
 export const USER_REGISTER_SUCCESS = 'USER_REGISTER_SUCCESS';
 export const USER_REGISTER_FAIL = 'USER_REGISTER_FAIL';
 
+export const USER_DETAILS_REQUEST = 'USER_DETAILS_REQUEST';
+export const USER_DETAILS_SUCCESS = 'USER_DETAILS_SUCCESS';
+export const USER_DETAILS_FAIL = 'USER_DETAILS_FAIL';
+
 export const userLoginReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_LOGIN_REQUEST:
@@ -32,6 +36,21 @@ export const userRegisterReducer = (state = {}, action) => {
       return { loading: false, error: action.payload };
     case USER_LOGOUT:
       return {};
+    default:
+      return state;
+  }
+};
+
+export const userDetailsReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
+    case USER_DETAILS_REQUEST:
+      return { ...state, loading: true };
+    case USER_DETAILS_SUCCESS:
+      return { loading: false, user: action.payload };
+    case USER_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+    case USER_LOGOUT:
+      return { user: {} };
     default:
       return state;
   }
---
+++ frontend/src/screens/ProfileScreen.jsx
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from './../components/Message';
+import Loader from './../components/Loader';
+import { getUserDetailsAction } from '../actions/userActions';
+import { USER_LOGOUT } from '../reducers/userReducers';
+
+const ProfileScreen = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [message, setMessage] = useState(null);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const userDetails = useSelector((state) => state.userDetails);
+  const { loading, error, user } = userDetails;
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  useEffect(() => {
+    if (!userInfo) navigate('/login');
+
+    if (!user.name) {
+      dispatch(getUserDetailsAction('profile'));
+    } else {
+      setName(user.name);
+      setEmail(user.email);
+    }
+  }, [userInfo, user, dispatch, navigate]);
+
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     if (error) {
+  //       dispatch({ type: USER_LOGOUT });
+  //     }
+  //   }, 3000);
+
+  //   return () => clearTimeout(timer);
+  // }, [error, dispatch]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setMessage('Пароли не совпадают');
+      setTimeout(() => setMessage(null), 3000);
+    } else {
+      // DISPATCH UPDATE PROFILE
+    }
+  };
+
+  return (
+    <Row>
+      <Col md={3}>
+        <h2>Профиль</h2>
+        {message && <Message variant="danger">{message}</Message>}
+        {error && <Message variant="danger">{error}</Message>}
+        {loading ? (
+          <Loader />
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="name">
+              <Form.Label>Логин</Form.Label>
+              <Form.Control
+                required
+                className="mb-2"
+                type="name"
+                placeholder="Введите логин"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="email">
+              <Form.Label>Электронная почта</Form.Label>
+              <Form.Control
+                required
+                className="mb-2"
+                type="email"
+                placeholder="Введите эл. адрес"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="password">
+              <Form.Label>Пароль</Form.Label>
+              <Form.Control
+                required
+                className="mb-2"
+                type="password"
+                placeholder="Введите новый пароль"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="confirmPassword">
+              <Form.Label>Подтвердите пароль</Form.Label>
+              <Form.Control
+                required
+                className="mb-2"
+                type="password"
+                placeholder="Подтвердите пароль"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Button type="submit" variant="primary">
+              Изменить
+            </Button>
+          </Form>
+        )}
+      </Col>
+
+      <Col md={9}>
+        <h2>Мои заказы</h2>
+      </Col>
+    </Row>
+  );
+};
+
+export default ProfileScreen;
