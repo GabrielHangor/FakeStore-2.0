15e6087405a08ca85a37aea1370533b5d5b675b4
Gabriel <gaba93@yandex.ru>
2022-01-11 12:41:56 +0300

    Order details reducer/action

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -4,6 +4,10 @@ export const ORDER_CREATE_FAIL = 'ORDER_CREATE_FAIL';
 export const ORDER_CREATE_CLEAN_ERROR = 'ORDER_CREATE_CLEAN_ERROR';
 export const ORDER_CREATE_CLEAN_SUCCESS = 'ORDER_CREATE_CLEAN_SUCCESS';
 
+export const ORDER_DETAILS_REQUEST = 'ORDER_DETAILS_REQUEST';
+export const ORDER_DETAILS_SUCCESS = 'ORDER_DETAILS_SUCCESS';
+export const ORDER_DETAILS_FAIL = 'ORDER_DETAILS_FAIL';
+
 export const orderCreateReducer = (state = {}, action) => {
   switch (action.type) {
     case ORDER_CREATE_REQUEST:
@@ -16,6 +20,22 @@ export const orderCreateReducer = (state = {}, action) => {
       return { ...state, error: null };
     case ORDER_CREATE_CLEAN_SUCCESS:
       return { ...state, success: null };
+    default:
+      return state;
+  }
+};
+
+export const orderDetailsReducer = (
+  state = { orderItems: [], shippingAddress: {} },
+  action
+) => {
+  switch (action.type) {
+    case ORDER_DETAILS_REQUEST:
+      return { ...state, loading: true };
+    case ORDER_DETAILS_SUCCESS:
+      return { loading: false, order: action.payload };
+    case ORDER_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
     default:
       return state;
   }
