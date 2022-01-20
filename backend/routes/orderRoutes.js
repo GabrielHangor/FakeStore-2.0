055050c0cf554fe78906926a3e0e7ef65dea385c
Gabriel <gaba93@yandex.ru>
2022-01-20 14:48:19 +0300

    get all orders route/controller

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,14 +4,18 @@ import {
   getOrderById,
   updateOrderToPaid,
   getMyOrders,
+  getAllOrders,
 } from '../controllers/orderController.js';
-import { protectRoute } from '../middleware/authMiddleware.js';
+import { protectRoute, verifyAdmin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 // routes handling user authentication/registration
 
-router.route('/').post(protectRoute, addOrderItems);
+router
+  .route('/')
+  .post(protectRoute, addOrderItems)
+  .get(protectRoute, verifyAdmin, getAllOrders);
 router.route('/myorders').get(protectRoute, getMyOrders);
 router.route('/:id').get(protectRoute, getOrderById);
 router.route('/:id/pay').put(protectRoute, updateOrderToPaid);
